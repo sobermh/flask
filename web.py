4e921ae59aea11221e1d2dfbff67734fb81857c3
hui mao <409788696@qq.com>
2022-05-16 17:53:39 +0800

    使用flask+mysql实现简单的注册登录功能

--- web.py
+++ web.py
@@ -6,6 +6,52 @@
 
 from flask import Flask, render_template, request
 import pymysql
+# 导入socket 库
+from socket import *
+from threading import Thread
+
+# 实例化一个socket对象 用来监听客户端连接请求
+listenSocket = socket(AF_INET, SOCK_STREAM)
+IP = '127.0.0.1'
+PORT = 3333
+BUFLEN = 512
+# socket绑定地址和端口
+listenSocket.bind((IP, PORT))
+# #设置为非阻塞
+# listenSocket.setblocking(False)
+listenSocket.listen(8)
+print(f'服务端启动成功，在{PORT}端口等待客户端连接...')
+
+# 这是新线程执行的函数，每个线程负责和一个客户端进行通信
+def clientHandler(dataSocket, addr):
+    while True:
+        recved = dataSocket.recv(BUFLEN)
+        # 当对方关闭连接的时候，返回空字符串
+        if not recved:
+            print(f'客户端{addr} 关闭了连接')
+            break
+
+        # 读取的字节数据是bytes类型，需要解码为字符串
+        global info
+        info = recved.decode()
+        print(f'收到{addr}信息： {info}')
+
+        dataSocket.send(f'服务端接收到了信息 {info}'.encode())
+    dataSocket.close()
+
+
+while True:
+    # 在循环中，一直接受新的连接请求
+    dataSocket, addr = listenSocket.accept()  # Establish connection with client.
+    addr = str(addr)
+    print(f'一个客户端 {addr} 连接成功')
+
+    # 创建新线程处理和这个客户端的消息收发
+    th = Thread(target=clientHandler, args=(dataSocket, addr))
+    th.start()
+    break
+listenSocket.close()
+
 
 app=Flask(__name__)
 
@@ -82,5 +128,9 @@ def js_do():
 @app.route("/jquery")
 def query_do():
     return render_template("jquery.html")
+
+@app.route("/fromtcp")
+def fromtcp():
+    return render_template("fromtcp.html",info=info)
 if __name__=='__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0',port=5000)
